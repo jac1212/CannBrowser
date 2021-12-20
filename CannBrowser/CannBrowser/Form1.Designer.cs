0db4d528803048a3388847b78ba19b3d391a838c
‘jac1212’ <ning_jackchen@163.com>
2021-12-20 14:32:45 +0800

    Signed-off-by: ‘jac1212’ <ning_jackchen@163.com>

---
+++ CannBrowser/CannBrowser/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CannBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.back = new System.Windows.Forms.PictureBox();
+            this.refresh = new System.Windows.Forms.PictureBox();
+            this.forward = new System.Windows.Forms.PictureBox();
+            this.url = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forward)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 30);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(837, 451);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // back
+            // 
+            this.back.Image = ((System.Drawing.Image)(resources.GetObject("back.Image")));
+            this.back.Location = new System.Drawing.Point(0, 2);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(23, 21);
+            this.back.TabIndex = 1;
+            this.back.TabStop = false;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.Location = new System.Drawing.Point(77, 1);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(21, 21);
+            this.refresh.TabIndex = 2;
+            this.refresh.TabStop = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // forward
+            // 
+            this.forward.Image = ((System.Drawing.Image)(resources.GetObject("forward.Image")));
+            this.forward.Location = new System.Drawing.Point(36, 2);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(22, 21);
+            this.forward.TabIndex = 3;
+            this.forward.TabStop = false;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // url
+            // 
+            this.url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.url.Location = new System.Drawing.Point(108, 3);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(729, 21);
+            this.url.TabIndex = 4;
+            this.url.Text = "https://";
+            this.url.KeyUp += new System.Windows.Forms.KeyEventHandler(this.url_KeyUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 483);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "柠萌工作室";
+            ((System.ComponentModel.ISupportInitialize)(this.back)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refresh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forward)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.PictureBox back;
+        private System.Windows.Forms.PictureBox refresh;
+        private System.Windows.Forms.PictureBox forward;
+        private System.Windows.Forms.TextBox url;
+    }
+}
+
---
+++ CannBrowser/CannBrowser/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CannBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("https://www.baidu.com/");
+            url.Text = "https://www.baidu.com/";
+        }
+        /// <summary>
+        /// 快捷键事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void url_KeyUp(object sender, KeyEventArgs e)
+        {
+            //回车
+            if (e.KeyCode == Keys.Enter)
+            {
+                string networking = "https://";
+                if (url.Text.IndexOf("https://") > -1 || url.Text.IndexOf("http://") > -1)
+                {
+                    networking = "";
+                }
+                webBrowser1.Navigate(networking + url.Text);
+            }
+            //最大最小窗口
+            if (e.KeyCode == Keys.F11)
+            {
+                if (WindowState == FormWindowState.Maximized)
+                    WindowState = FormWindowState.Normal;
+                else
+                    WindowState = FormWindowState.Maximized;
+            }
+            //刷新
+            if (e.KeyCode == Keys.F5)
+            {
+                webBrowser1.Refresh();
+            }
+        }
+        private void back_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();//后退
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();//刷新
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();//前进
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            url.Text = ((WebBrowser)sender).Document.Window.Url.OriginalString;
+            ((WebBrowser)sender).Document.Window.Error += new HtmlElementErrorEventHandler(Window_Error);
+            //将所有的链接的目标，指向本窗体
+            foreach (HtmlElement archor in this.webBrowser1.Document.Links)
+            {
+                archor.SetAttribute("target", "_self");
+            }
+
+            //将所有的FORM的提交目标，指向本窗体
+            foreach (HtmlElement form in this.webBrowser1.Document.Forms)
+            {
+                form.SetAttribute("target", "_self");
+            }
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+        private void Window_Error(object sender, HtmlElementErrorEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
